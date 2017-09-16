b792e3ec91b5510724e37914402d6bf9a538bdf8
WebGao <929817104@qq.com>
2017-09-16 17:36:12 +0800

    修改search

--- static/search_track/js/index.js
+++ static/search_track/js/index.js
@@ -1,20 +1,20 @@
-/**
- * Created by Administrator on 2017/1/5.
- */
-angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('PaginationDemoCtrl', function ($scope, $log) {
-    $scope.totalItems = 64;
-    $scope.currentPage = 4;
+// /**
+//  * Created by Administrator on 2017/1/5.
+//  */
+// angular.module('searchListApp', ['ui.bootstrap']);
+// angular.module('searchListApp').controller('searchListController', function ($scope, $log) {
+//     $scope.totalItems = 64;
+//     $scope.currentPage = 1;
 
-    $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-    };
+//     $scope.setPage = function (pageNo) {
+//         $scope.currentPage = pageNo;
+//     };
 
-    $scope.pageChanged = function() {
-        $log.log('Page changed to: ' + $scope.currentPage);
-    };
+//     $scope.pageChanged = function() {
+//         $log.log('Page changed to: ' + $scope.currentPage);
+//     };
 
-    $scope.maxSize = 5;
-    $scope.bigTotalItems = 175;
-    $scope.bigCurrentPage = 1;
-});
+//     $scope.maxSize = 5;
+//     $scope.bigTotalItems = 175;
+//     $scope.bigCurrentPage = 1;
+// });
